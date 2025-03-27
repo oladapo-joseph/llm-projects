d041add3ff3d45c38b955dd9033aa1324de19b12
Oladapo Adeleke <oladapo_joseph@yahoo.com>
2025-03-27 01:53:36 +0100

    updated youtube assistant and made changes to conceptHelper/prompter

--- youtubeAssistant/app.py
+++ youtubeAssistant/app.py
@@ -1,40 +1,122 @@
 import streamlit as st
-from youtubeTranscript_OpenAI import getSummary, getResponse,create_Youtube_vectors
+from youtubeTranscript_OpenAI import getSummary, getResponse,embeddings, template
+import gc 
+import uuid
+from youtube_transcript_api import YouTubeTranscriptApi
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
+from langchain_core.prompts import PromptTemplate 
+from langchain_community.vectorstores import  FAISS 
+from youtube_transcript_api import YouTubeTranscriptApi 
+from dotenv import load_dotenv 
+
+
+load_dotenv('../.env')
 
 st.title('Youtube Assistant')
-st.subheader('Here to help you with your videos')
 
-with st.sidebar:
-    with st.form(key='ikform'):
-        url = st.sidebar.text_input('Share the Url of the Youtube video', placeholder='https://youtube.com/xxxxxxxx',)
+
+
+if "id" not in st.session_state:
+    st.session_state.id = uuid.uuid4()
+    st.session_state.file_cache = {}
+
+session_id = st.session_state.id
+client = None
+
+
 
 query = ''
 vectorDb =''
-if url:
-    with st.spinner('Fetching Transcript'):
-        vectorDb, summary = create_Youtube_vectors(url)
-        st.markdown(f'You requested to know about this {url}')
-        st.markdown('---')
-        st.markdown(f'Video Summary:')
-        st.markdown(f'{getSummary(summary)}')
-        st.markdown('---')
-        st.markdown('What would you like to know about the video?')
-        query = st.sidebar.text_area("You can type your question here")
-else:
-    st.info('Please enter a Url link')
 
-if query:
-    response =getResponse(vectorDb, query)
-    with st.spinner('Answering your question'):
-        try: 
-            st.session_state['prompt_result'] = response 
+def create_Youtube_vectors(url:str)->FAISS: 
+    
+    def getId(url)->str:
+        from urllib.parse import urlparse, parse_qs
+        if url.startswith('http'):
+            query = urlparse(url).query
+            params = parse_qs(query)
+            return params.get('v', [url.split('/')[-1]])[0]
+        return url
+    
+    transcript= YouTubeTranscriptApi.get_transcript(getId(url))
+    
+    full_transcript = " ".join([i['text'] for i in transcript])
+
+    # next thing is to split the transcript into documents
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, 
+                                                   chunk_overlap=200)
+    docs = text_splitter.split_text(full_transcript)
+
+    db = FAISS.from_texts(docs, embeddings)
+
+    return db, docs
+
+def reset_chat():
+    st.session_state.messages = []
+    st.session_state.context = None
+    gc.collect()
+
+
+with st.sidebar:
+    url = st.sidebar.text_input('Share the Url of the Youtube video', placeholder='https://youtube.com/xxxxxxxx',)
+    if url:
+        try:
+            with st.spinner('Fetching Transcript'):
+                vectorDb, summary = create_Youtube_vectors(url)
+                st.markdown(f'You requested to know about this {url}')
+                st.session_state.file_cache['summary'] = getSummary(summary)
+                st.success("Ready to Chat! View Summary Below")
+                st.markdown(st.session_state.file_cache['summary'])
+                
         except Exception as e:
-            st.error(f'Error fetching response, {str(e)}')
+            st.error(f"An error occurred: {e}")
+            st.stop()     
 
-    if 'prompt_result' in st.session_state:
-        st.markdown(st.session_state['prompt_result'])
-    else:
-        st.info("What would you like to know about the video.")
+col1, col2 = st.columns([6, 1])
+
+with col1:
+    st.subheader("Chat with the AI")
+
+with col2:
+    st.button("Clear ↺", on_click=reset_chat)
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    reset_chat()
 
 
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+# Accept user input
+if prompt := st.chat_input("What's up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        
+        # Simulate stream of response with milliseconds delay
+        streaming_response = getResponse(vectorDb, prompt)
+        
+        for chunk in streaming_response.split(' '):
+            full_response += chunk
+            full_response += ' '
+            message_placeholder.markdown(full_response + "▌")
+
+        
+
+        message_placeholder.markdown(full_response)
+        # st.session_state.context = ctx
+
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
